c2dc250f02a306d8534af0f53c59fcb88b0b047d
Sullen-ui <maorlov.it@gmail.com>
2022-03-17 14:35:44 +0700

    test

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 echo killing old docker processes
-/Counting-pixels/docker-compose rm -fs
+docker-compose rm -fs
 
 echo building docker containers
-/Counting-pixels/docker-compose up --build -d
+docker-compose up --build -d
 
